7c2556efbc2a3897f4bb780dc18c1035781377c9
arpankumarde <134305585+arpankumarde@users.noreply.github.com>
2025-03-17 22:46:59 +0530

    added recruiter dashboard

--- src/app/recruiter/dashboard/page.tsx
+++ src/app/recruiter/dashboard/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+// import prisma from "@/lib/prisma";
+import { InterviewTypeChart } from "./_components/InterviewTypeChart";
+import { JobApplicationsLongChart } from "./_components/JobApplicationsLongChart";
+import { JobPostByMonth } from "./_components/JobPostByMonth";
+import { JobVsPostings } from "./_components/JobVsPostings";
 
-const page = () => {
+const Page = async () => {
+  // const interviews = await prisma.interview.findMany({
+  //   select: {
+  //     conductOffline: true,
+  //     conductWithAI: true,
+  //   },
+  // });
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="p-4 md:p-6 space-y-4">
+      <div>
+        <h1 className="text-4xl font-semibold text-primary">Dashboard</h1>
+      </div>
 
-export default page
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+        <div>
+          <InterviewTypeChart />
+        </div>
+        <div>
+          <JobVsPostings />
+        </div>
+        <div>
+          <JobPostByMonth />
+        </div>
+      </div>
+
+      <div>
+        <JobApplicationsLongChart />
+      </div>
+    </div>
+  );
+};
+
+export default Page;
